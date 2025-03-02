c0b4f8550d556fd0590231c2b8a8184d0f82fd37
Mahmoud Sameh <Mahmoud.sameh@itida.gov.eg>
2025-03-02 17:33:11 +0200

    Add UserCompetenciesDtoResponse

---
+++ PerformanceManagementSystem.Application/DTOs/UserCompetenciesDtoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceManagementSystem.Application.DTOs
+{
+    public class UserCompetenciesDtoResponse
+    {
+        public int Year { get; set; }
+        public int StageID { get; set; }
+        public List<UserCompetencyDto> Competencies { get; set; }
+    }
+}
